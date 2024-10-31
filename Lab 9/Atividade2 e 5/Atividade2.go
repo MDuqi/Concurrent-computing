fc1015cf0bc9c2f0b0cb8a5a17f2b7222a3b3fcd
Duque <150629749+MDuqi@users.noreply.github.com>
2024-10-31 16:47:42 -0300

    Adição Atividade2.go

---
+++ Lab 9/Atividade2 e 5/Atividade2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func tarefa(str chan string) {
+	msg := <-str
+	fmt.Println("Goroutine recebeu:", msg)
+
+	str <- "Oi Main, bom dia, tudo bem?"
+
+	msg = <-str
+	fmt.Println("Goroutine recebeu:", msg)
+
+	str <- "Certo, entendido."
+
+	fmt.Println("Goroutine: finalizando")
+}
+
+func main() {
+	str := make(chan string)
+
+	go tarefa(str)
+
+	str <- "Olá, Goroutine, bom dia!"
+	msg := <-str
+	fmt.Println("Main recebeu:", msg)
+
+	str <- "Tudo bem! Vou terminar tá?"
+	msg = <-str
+	fmt.Println("Main recebeu:", msg)
+
+	fmt.Println("Main: finalizando")
+}
+
